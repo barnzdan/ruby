62fa9e10a52027d1749708531d6c95f4e2b85efb
Dan Barnes <barnzdan@gmail.com>
2013-05-04 12:45:46 -0400

    update

--- rails/fooapp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Fooapp::Application.config.secret_token = '27996013e4e6729a98f6cdfabe920c8c3c473086d3814af56170267c51e5c80e91e15bc3519f518d6ed0393f21ea061838b9c22692792faa4873f2fbea38c0bf'
