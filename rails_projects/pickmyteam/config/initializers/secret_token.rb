276c7527023e2c39167370adb8c9decb519f86e7
Dan Barnes <barnzdan@gmail.com>
2013-09-02 16:13:22 -0400

    pmt

---
+++ rails_projects/pickmyteam/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pickmyteam::Application.config.secret_key_base = '469e92f7af7368a2f4355e0a70d5201f30dab42ae55404c97143c9f81e2f6c806f8f2ed2cbf4d57022da763e32e5a5a5af999fd7c5fbfb2ed88ebde8b567764e'
